c79f5d68c7b4a5b42e6db9765566e30c77f361a7
smazoomder@gmail.com <smazoomder@gmail.com>
2019-11-26 13:01:56 +0600

    third

---
+++ README.md
@@ -0,0 +1,12 @@
+# Utility Functions for developers
+Author: Shaon Majumder
+
+## Utilities
+- stats
+- network
+- image
+- string
+- file
+
+## Installation
+	pip install shaonutil
--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'shaonutil',
   packages = ['shaonutil'],
-  version = '0.1',
+  version = '0.2',
   description = 'collection of utility by shaon',
   author = 'Shaon Majumder',
   author_email = 'smazoomder@gmail.com',
   url = 'https://github.com/ShaonMajumder/shaonutil',
-  download_url = 'https://github.com/ShaonMajumder/shaonutil/archive/0.1.tar.gz',
+  download_url = 'https://github.com/ShaonMajumder/shaonutil/archive/0.2.tar.gz',
   keywords = ['shaon', 'utility', 'statistics'], 
   classifiers = [],
 )
