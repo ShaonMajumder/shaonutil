29d5805d10256b0bd969be59d9a5eabdcb6f11eb
shaon <smazoomder@gmail.com>
2019-11-26 22:38:32 +0600

    mode

--- examples/statistics.py
+++ examples/statistics.py
@@ -1,3 +1,5 @@
 import shaonutil
-print("average = ",shaonutil.stats.mean([1,2.5]))
-print("median = ",shaonutil.stats.median([1,2.5,4,3]))
+li = [1, 2, 3, 4, 5, 5, 6, 6]
+print("average = ",shaonutil.stats.mean(li))
+print("median = ",shaonutil.stats.median(li))
+print("mode = ",shaonutil.stats.median(li))
--- shaonutil/stats.py
+++ shaonutil/stats.py
@@ -1,3 +1,16 @@
+def counter(li,number):
+	num = 0
+	for c in li:
+		if c == number:
+			num += 1
+	return num
+
+def occurance_dic(li):
+	dic = {}
+	for number in li:
+		dic[number] = counter(li,number)
+	return dic
+
 def mean(li):
 	# avearage or mean of elements
 	return sum(li)/len(li)
@@ -16,6 +29,16 @@ def median(li):
 
 	return median
 
+def mode(li):
+	# mode of elements 
+	n = len(li)
+	data = occurance_dic(li)
+	mode = [k for k, v in data.items() if v == max(list(data.values()))] 
+	  
+	if len(mode) == n: 
+	    raise ValueError('No mode found !')
+	else:
+	    return mode
 
 
 if __name__ == '__main__':
