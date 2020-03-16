e9b0d9bf78e85a94693f653964160f5336ca2652
Shaon Majumder <smazoomder@gmail.com>
2020-03-16 11:56:11 +0600

    shaonutil

--- shaonutil/strings.py
+++ shaonutil/strings.py
@@ -23,5 +23,17 @@ def randomString(stringLength=10):
 
     return ''.join(random.choice(letters) for i in range(stringLength))
 
+def generateSecureRandomString(stringLength=10):
+    """Generate a secure random string of letters, digits and special characters """
+    password_characters = string.ascii_letters + string.digits #+ string.punctuation
+    return ''.join(secrets.choice(password_characters) for i in range(stringLength))
+
+def generateCryptographicallySecureRandomString(stringLength=10):
+	randomString = uuid.uuid4().hex.upper() # get a random string in a UUID fromat
+	randomString  = randomString[0:stringLength]
+	return randomString
+
+
+
 if __name__ == '__main__':
 	pass
