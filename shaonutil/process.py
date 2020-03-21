192e5a0900008734f6ec9027a0db830f726d02cc
Shaon Majumder <smazoomder@gmail.com>
2020-03-22 01:58:33 +0600

    mysql pid removed;

--- shaonutil/process.py
+++ shaonutil/process.py
@@ -4,4 +4,4 @@ import subprocess
 
 def start_mysql_server(mysql_bin_folder,mysql_config_file):
 	DETACHED_PROCESS = 0x00000008	
-	return subprocess.Popen([os.path.join(mysql_bin_folder,"mysqld.exe"),"--defaults-file="+mysql_config_file,"--standalone"],creationflags=DETACHED_PROCESS).pid
+	return subprocess.Popen([os.path.join(mysql_bin_folder,"mysqld.exe"),"--defaults-file="+mysql_config_file,"--standalone"],creationflags=DETACHED_PROCESS)
