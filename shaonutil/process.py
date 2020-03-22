eb7c10f7504114a212d6f277fd056a6e90e92c56
Shaon Majumder <smazoomder@gmail.com>
2020-03-22 17:06:45 +0600

    0.0.0.40.1;

--- shaonutil/process.py
+++ shaonutil/process.py
@@ -9,6 +9,7 @@ def start_mysql_server(mysql_bin_folder,mysql_config_file):
 
 def get_mysql_datadir(mysql_bin_folder,user,pass_=''):
 	"""Get mysql data directory"""
+	
 	process = subprocess.Popen([os.path.join(mysql_bin_folder,"mysql"),"--user="+user,"--password="+pass_,"-e","select @@datadir;"],stdout=subprocess.PIPE)
 	out, err = process.communicate()
 	out = [line for line in out.decode('utf8').replace("\r\n","\n").split('\n') if line != ''][-1]
